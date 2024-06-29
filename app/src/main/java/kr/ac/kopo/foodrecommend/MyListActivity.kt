9da78c0a7e77fc5b6c24207d8c4b76a2336e9a24
DeveloperYeti <hoseok9823@gmail.com>
2024-06-29 15:37:42 +0900

    sidePage 작성하기

---
+++ app/src/main/java/kr/ac/kopo/foodrecommend/MyListActivity.kt
@@ -0,0 +1,11 @@
+package kr.ac.kopo.foodrecommend
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MyListActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.mylist)  // mylist.xml을 레이아웃으로 설정
+    }
+}
